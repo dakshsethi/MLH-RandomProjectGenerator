ceb540939fe7aa7120d93be2253448512df7b4b2
Atisheel-Yadav <84717982+Atisheel-Yadav@users.noreply.github.com>
2021-10-14 00:37:23 -0700

    updated

--- src/projectList.js
+++ src/projectList.js
@@ -19,6 +19,45 @@ const projectList = [
         id: 5,
         idea: "File Manager"
     },
-];
-
+    {
+      id: 6,
+      idea: "Colour flipper"
+    },
+    {
+      id: 7,
+      idea: "Calculator"
+    },
+    {
+        id: 8,
+        idea:"Responsive Navbar"
+    },
+    {
+        id:9,
+        idea:"Meme Generator"
+    },
+    {
+        id:10,
+        idea:"Calorie tracker"
+    },
+    {
+        id:11,
+        idea:"chess board"
+    },
+    {
+        id:12,
+        idea:"Tic-Tac-Toe"
+    },
+    {
+        id:13,
+        idea:"Drum-Kit"
+    },
+    {
+        id:14,
+        idea:"Quiz app"
+    },
+    {
+        id:15,
+        idea:"Random dog generator"
+    }
+  ];
 export default projectList;
